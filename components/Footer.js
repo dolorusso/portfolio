cfea0c8d804b7a94bd01276054ea4de8c1f49186
Dolores Russo <drusso@alumnos.exa.unicen.edu.ar>
2025-04-30 10:57:02 -0300

    cambios mobile

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,10 @@ import { FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full pt-4 pb-4 bg-transparent backdrop-blur-md text-white z-10">
-      <div className="w-full flex justify-end pr-32">
+      <div className="w-full flex justify-end pr-16 md:pr-32">
         <div className="flex justify-end space-x-6">
-          {[{ icon: FaLinkedin, href: 'https://www.linkedin.com/in/dolores-russo-390b5127a/' },
+          {[
+            { icon: FaLinkedin, href: 'https://www.linkedin.com/in/dolores-russo-390b5127a/' },
             { icon: FaTwitter, href: 'https://x.com/dolorusso1' },
             { icon: FaInstagram, href: 'https://www.instagram.com/dolo_russo/' },
           ].map((social, index) => (
